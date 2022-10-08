29a82bcf9cb624fc8525386436e6f6e76f29fe74
Zaksley <enzo8550@gmail.com>
2022-10-08 14:07:37 -0400

    Proper end scene for level 1

---
+++ Assets/Scripts/Platformer/GateRendererController.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+
+public class GateRendererController : MonoBehaviour
+{
+    // Components 
+    private SpriteRenderer _spriteRenderer;
+
+    // Sprite
+    [SerializeField] private Sprite _spriteEndLevel; 
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        GameManager.OnPlayerGotKey += UpdateSprite; 
+    }
+
+    private void UpdateSprite(object sender, EventArgs args)
+    {
+        _spriteRenderer.sprite = _spriteEndLevel; 
+    }
+}
