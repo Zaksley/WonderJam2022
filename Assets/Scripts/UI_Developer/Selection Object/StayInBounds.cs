611f8f2c588b0e7b625edc23143c583baed92797
Pierre-Jean MOREL <pj33720@gmail.com>
2022-10-08 12:51:26 -0400

    Tweakable outline

--- Assets/Scripts/UI_Developer/Selection Object/StayInBounds.cs
+++ Assets/Scripts/UI_Developer/Selection Object/StayInBounds.cs
@@ -1,25 +1,38 @@
 using System;
-using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(LineRenderer))]
 public class StayInBounds : MonoBehaviour
 {
     [SerializeField] private Vector2 _bounds;
     [SerializeField] private Color _gizmoColor = new Color(1.0f, 0.0f, 0.0f, 0.25f);
 
+    private LineRenderer _lines;
     private Vector2 _initialPosition;
     private Vector2 _spriteSize;
 
+    public bool ShowOutline
+    {
+        get => _lines.enabled;
+        set => _lines.enabled = value;
+    }
+    
     private void Start()
     {
         _initialPosition = gameObject.transform.position;
         _spriteSize = transform.GetComponent<Collider2D>().bounds.size / 2.0f;
-    }
 
-    private void Awake()
-    {
-        _initialPosition = gameObject.transform.position;
+        var leftUpCorner = _initialPosition - _bounds;
+        var rightDownCorner = _initialPosition + _bounds;
+        _lines = GetComponent<LineRenderer>();
+        _lines.SetPositions(new Vector3[]
+        {
+            new(leftUpCorner.x, leftUpCorner.y, 0.0f),
+            new(rightDownCorner.x, leftUpCorner.y, 0.0f),
+            new(rightDownCorner.x, rightDownCorner.y, 0.0f),
+            new(leftUpCorner.x, rightDownCorner.y, 0.0f),
+        });
     }
 
     private void LateUpdate()
