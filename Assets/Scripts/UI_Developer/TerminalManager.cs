cfc1e222287b9c26a209a210b9e9a76c1bcb6157
Hugo Langlais <bedjestudio@gmail.com>
2022-10-08 11:31:15 -0400

    Pixel font + starting color in terminal

--- Assets/Scripts/UI_Developer/TerminalManager.cs
+++ Assets/Scripts/UI_Developer/TerminalManager.cs
@@ -1,38 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class TerminalManager : MonoBehaviour
 {
-
     // References to our prefab
-    public GameObject text;
+    public GameObject textPrefab;
     public ScrollRect sr;
     public GameObject linesContainer;
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Return)) {
-        AddLine("HELLO");
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Log("HELLO");
         }
     }
 
-    private void AddLine(string newText)
+    public void Log(string text)
     {
         // Scale scroll rect depending on the content
         Vector2 linesContainerSize = linesContainer.GetComponent<RectTransform>().sizeDelta;
         linesContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(linesContainerSize.x, linesContainerSize.y + 30.0f);
 
         // Create a new line
-        GameObject newLine = Instantiate(text, linesContainer.transform);
+        GameObject newLine = Instantiate(textPrefab, linesContainer.transform);
 
         // Set child index to respect the console line order
-        newLine.transform.SetSiblingIndex(linesContainer.transform.childCount -1);
+        newLine.transform.SetSiblingIndex(linesContainer.transform.childCount - 1);
 
         // Set the text
-        newLine.GetComponentInChildren<TextMeshProUGUI>().text = newText;
+        newLine.GetComponentInChildren<TextMeshProUGUI>().text = FormatConsoleText(text);
+
     }
 
+    private string FormatConsoleText(string text)
+    {
+        System.DateTime currentTime = System.DateTime.Now;
+        return "<color=#FF0000>" + currentTime.Hour.ToString() + "</color>" ;
+    }
 }
