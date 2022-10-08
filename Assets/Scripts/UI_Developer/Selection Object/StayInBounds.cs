4c00e62731ac0c9f1c6b4b50bed99a62c18398b8
Pierre-Jean MOREL <pj33720@gmail.com>
2022-10-08 10:48:29 -0400

    Fix gizmo for bounds

--- Assets/Scripts/UI_Developer/Selection Object/StayInBounds.cs
+++ Assets/Scripts/UI_Developer/Selection Object/StayInBounds.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class StayInBounds : MonoBehaviour
 {
-    [SerializeField] private Bounds _bounds;
+    [SerializeField] private Vector2 _bounds;
     [SerializeField] private Color _gizmoColor = new Color(1.0f, 0.0f, 0.0f, 0.25f);
 
+    private Vector2 _initialPosition;
     private Vector2 _spriteSize;
 
     private void Start()
     {
+        _initialPosition = gameObject.transform.position;
         _spriteSize = transform.GetComponent<SpriteRenderer>().bounds.size / 2.0f;
+    }
+
+    private void Awake()
+    {
+        _initialPosition = gameObject.transform.position;
     }
 
     private void LateUpdate()
@@ -20,14 +27,14 @@ public class StayInBounds : MonoBehaviour
         var gameObjectPos = gameObject.transform.position;
 
         gameObject.transform.position = new Vector3(
-            Mathf.Clamp(gameObjectPos.x, _bounds.min.x + _spriteSize.x, _bounds.max.x - _spriteSize.x),
-            Mathf.Clamp(gameObjectPos.y, _bounds.min.y + _spriteSize.y, _bounds.max.y - _spriteSize.y),
+            Mathf.Clamp(gameObjectPos.x,  _initialPosition.x - _bounds.x + _spriteSize.x, _initialPosition.x + _bounds.x - _spriteSize.x),
+            Mathf.Clamp(gameObjectPos.y, _initialPosition.y - _bounds.y + _spriteSize.y, _initialPosition.y + _bounds.y - _spriteSize.y),
             gameObjectPos.z);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = _gizmoColor;
-        Gizmos.DrawCube(_bounds.center, _bounds.size);
+        Gizmos.DrawCube(Application.isPlaying ?  _initialPosition : gameObject.transform.position, _bounds * 2.0f);
     }
 }
