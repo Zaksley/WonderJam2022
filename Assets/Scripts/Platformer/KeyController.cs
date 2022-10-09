8bf940d07cd9d32ceecfea2dbde7167cf3265313
Lanth <perig.herau@gmail.com>
2022-10-09 11:53:49 -0400

    Key animation, pickup sound, platform glitch on drag

--- Assets/Scripts/Platformer/KeyController.cs
+++ Assets/Scripts/Platformer/KeyController.cs
@@ -1,18 +1,23 @@
 using System;
 using UnityEngine;
-
+using System.Collections;
 
 [RequireComponent(typeof(Collider2D))]
 public class KeyController : MonoBehaviour
 {
+    public float floatFrequency = .5f;
+    public float floatAmplitude = .01f;
+
     private void Start()
     {
+        StartCoroutine(FloatAnimation());
         GameManager.OnPlayerDie += RestartKey; 
     }
 
     private void RestartKey(object sender, EventArgs args)
     {
         gameObject.SetActive(true);
+        StartCoroutine(FloatAnimation());
     }
     
     private void OnTriggerEnter2D(Collider2D col)
@@ -23,6 +28,19 @@ public class KeyController : MonoBehaviour
         {
             player.PlayerGotKey();
             gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator FloatAnimation()
+    {
+        // Sine-like floating animation
+        Vector3 tmp = new Vector3();
+        while(true)
+        {
+            tmp = transform.position;
+            tmp.y = (float) Math.Sin(Time.fixedTime * Math.PI * floatFrequency) * floatAmplitude * .3f;
+            transform.position = tmp;
+            yield return null;
         }
     }
 
--- Assets/Scripts/UI_Developer/MoveObjectByCircle.cs
+++ Assets/Scripts/UI_Developer/MoveObjectByCircle.cs
@@ -6,10 +6,23 @@ public class MoveObjectByCircle : MonoBehaviour
     [SerializeField] private Transform _parentToRotate;
     [SerializeField] private float _speed = 5.0f;
 
+    public Sprite GlitchedSprite;
+
     private float _previousRotation;
+    private bool _glitched;
+
+    void Start()
+    {
+        _glitched = false;
+    }
 
     private void OnMouseDrag()
     {
+        if(!_glitched)
+        {
+            SwitchToGlitchedSprite();
+            _glitched = true;
+        }
         _parentToRotate.rotation = Quaternion.AngleAxis( _previousRotation + (MouseWorldPosition().y - _parentToRotate.position.y) * _speed, Vector3.forward);
     }
 
@@ -23,5 +36,11 @@ public class MoveObjectByCircle : MonoBehaviour
         var mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Camera.main.WorldToScreenPoint(_parentToRotate.position).z;
         return Camera.main.ScreenToWorldPoint(mouseScreenPos); 
+    }
+
+    private void SwitchToGlitchedSprite()
+    {
+        Debug.Log("Glitched now");
+        transform.parent.GetComponent<SpriteRenderer>().sprite = GlitchedSprite;
     }
 }
