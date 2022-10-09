83afc83d83b202c2c9bfba5477c5a770c3f57c53
Pierre-Jean MOREL <pj33720@gmail.com>
2022-10-09 11:45:21 -0400

    Added respawn

--- Assets/Scripts/Platformer/DieZoneController.cs
+++ Assets/Scripts/Platformer/DieZoneController.cs
@@ -6,8 +6,12 @@ public class DieZoneController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {        
         var player = col.gameObject.GetComponent<PlayerController>();
+        var explodable = col.gameObject.GetComponent<RespawnController>();
 
         if (player != null)
             player.Die();
+        
+        if (explodable != null)
+            explodable.Explode();
     }
 }
