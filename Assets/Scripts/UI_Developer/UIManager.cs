00c905fe9c6a48863b6d579d725e15e568a0f0ce
Marion0401 <71750635+Marion0401@users.noreply.github.com>
2022-10-09 11:17:23 -0400

    UI Menu

---
+++ Assets/Scripts/UI_Developer/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    //[SerializeField] AudioSource AudioGeneral;
+    //[SerializeField] GameObject SoundOn;
+    //[SerializeField] GameObject SoundOff;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+    //public void CutSound()
+    //{
+    //    AudioGeneral.;
+    //}
+
+    //public void PlayerSound()
+    //{
+    //    AudioGeneral.Play();
+    //}
+}
